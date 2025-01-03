e95d1a94d10206478cf90cee04669f1087a1f352
Andreas Presthammer <apres@equinor.com>
2025-01-03 14:52:30 +0100

    refactor(markdown): Initialize 'markdown-viewer' using a proper 'EditorView' instance
    This will allow us to configure the editorprops which will allow upcoming commits to control the rendering capabilities of the markdown-viewer.

--- packages/markdown/src/markdown-viewer/element.ts
+++ packages/markdown/src/markdown-viewer/element.ts
@@ -1,8 +1,9 @@
-import { css, type CSSResult, html, LitElement, type PropertyValues, type TemplateResult } from 'lit';
-import { property, query } from 'lit/decorators.js';
-import { unsafeHTML } from 'lit/directives/unsafe-html.js';
+import { css, type CSSResult, html, LitElement, type TemplateResult } from 'lit';
+import { property, query, queryAsync } from 'lit/decorators.js';
 import { MarkdownViewerElementProps } from './types';
 import { defaultMarkdownParser } from 'prosemirror-markdown';
+import { EditorView } from 'prosemirror-view';
+import { EditorState } from 'prosemirror-state';
 
 const styles = css`
   slot {
@@ -16,39 +17,43 @@ const styles = css`
 export class MarkdownViewerElement extends LitElement implements MarkdownViewerElementProps {
   static styles: CSSResult = styles;
 
-  @property({
-    type: String,
-    reflect: false,
-    attribute: true,
-  })
-  public value = '';
+  @property({ type: String, reflect: true })
+  value = '';
 
-  /**
-   * @internal
-   */
-  protected content = '';
+  @queryAsync('#viewer')
+  viewer!: Promise<HTMLDivElement>;
 
-  /**
-   * @internal
-   */
+  protected view!: EditorView;
+
   @query('slot')
   mainSlot!: HTMLSlotElement;
 
-  protected updated(props: PropertyValues): void {
-    if (props.has('value') && this.value !== props.get('value')) {
-      this.content = defaultMarkdownParser.tokenizer.render(this.value);
-      this.requestUpdate('content');
-    }
+  connectedCallback() {
+    super.connectedCallback();
+    this.initializeViewer();
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.view && this.view.destroy();
+  }
+
+  private async initializeViewer() {
+    this.view = new EditorView(await this.viewer, {
+      state: EditorState.create({
+        doc: defaultMarkdownParser.parse(this.value),
+      }),
+    });
   }
 
   protected handleSlotChange(): void {
     this.value = this.mainSlot.assignedNodes()[0]?.textContent || '';
   }
 
-  protected render(): TemplateResult {
+  public render(): TemplateResult {
     return html`
       <slot @slotchange=${this.handleSlotChange}></slot>
-      <div id="content">${unsafeHTML(this.content)}</div>
+      <div id="viewer"></div>
     `;
   }
 }
